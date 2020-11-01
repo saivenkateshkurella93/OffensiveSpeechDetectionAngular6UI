eb3120b3df063c5e3d5ada5c4939c7754f1e471b
sai venkatesh kurella <Sai Venkatesh Kurella>
2020-11-01 01:02:34 -0400

    Project setup with all files.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,18 +3,31 @@ import { first } from 'rxjs/operators';
 
 import { User } from '../_models';
 import { UserService } from '../_services';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({templateUrl: 'home.component.html'})
 export class HomeComponent implements OnInit {
     currentUser: User;
     users: User[] = [];
+    rest = "http://localhost:8080/predict?text="
+    products: any;
 
-    constructor(private userService: UserService) {
+    constructor(private userService: UserService, private httpClient: HttpClient) {
         this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
     }
 
     ngOnInit() {
         this.loadAllUsers();
+        this.httpClient.get(this.rest+"heyy you whats up").subscribe(
+            data  => {
+            console.log("POST Request is successful ", data);
+            },
+            error  => {
+            
+            console.log("Error", error);
+            
+            });
     }
 
     deleteUser(id: number) {
@@ -22,6 +35,14 @@ export class HomeComponent implements OnInit {
             this.loadAllUsers() 
         });
     }
+
+    // getProducts() {
+    //     this.products = [];
+    //     this.rest.getProducts().subscribe((data: {}) => {
+    //       console.log(data);
+    //       this.products = data;
+    //     });
+    //   }
 
     private loadAllUsers() {
         this.userService.getAll().pipe(first()).subscribe(users => { 
